aec04f5721c3c4da0956f68b23c418ff1d1fba02
John Spurlock <john@skymethod.com>
2022-09-09 11:03:57 -0500

    Update deployment deno to latest version and deps to latest std.

--- redirect_episode_test.ts
+++ redirect_episode_test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.154.0/testing/asserts.ts';
+import { assertEquals } from 'https://deno.land/std@0.155.0/testing/asserts.ts';
 import { tryParseRedirectRequest, computeRedirectResponse } from './redirect_episode.ts';
 
 Deno.test({
