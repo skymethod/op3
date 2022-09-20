36bf032f43cb141f802bb2ffb99c8b75a9965b2f
John Spurlock <john@skymethod.com>
2022-09-20 13:50:32 -0500

    Update test.

--- worker/routes/api_query_redirect_logs_test.ts
+++ worker/routes/api_query_redirect_logs_test.ts
@@ -1,12 +1,19 @@
-import { assertEquals, assertStringIncludes } from 'https://deno.land/std@0.155.0/testing/asserts.ts';
-import { RpcClient } from '../rpc_model.ts';
+import { assertEquals } from 'https://deno.land/std@0.155.0/testing/asserts.ts';
+import { QueryRedirectLogsRequest, Unkinded } from '../rpc_model.ts';
 import { computeQueryRedirectLogsResponse } from './api_query_redirect_logs.ts';
 
 Deno.test({
     name: 'computeQueryRedirectLogsResponse',
     fn: async () => {
-        const res = await computeQueryRedirectLogsResponse('GET', new URLSearchParams({ url: 'https://example.com/path/to/ep.mp3*'}), {} as RpcClient);
-        assertEquals(res.status, 400);
-        assertStringIncludes(await res.text(), 'not implemented yet');
+        const queryRedirectLogs = (request: Unkinded<QueryRedirectLogsRequest>, target: string) => {
+            if (request.urlStartsWith === 'https://example.com/path/to/ep.mp3' && target === 'combined-redirect-log') {
+                return new Response('ok!');
+            }
+            console.log(request, target);
+            throw new Error();
+        }
+        // deno-lint-ignore no-explicit-any
+        const res = await computeQueryRedirectLogsResponse('GET', new URLSearchParams({ url: 'https://example.com/path/to/ep.mp3*'}), { queryRedirectLogs } as any);
+        assertEquals(res.status, 200);
     }
 });
