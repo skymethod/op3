863c934a6d5821ff3b9290947429799f69654e86
John Spurlock <john@skymethod.com>
2022-09-27 17:26:57 -0500

    Fix the home page footer layout on narrow screens.

--- worker/routes/home.ts
+++ worker/routes/home.ts
@@ -6,7 +6,7 @@ const homeHtm = await importText(import.meta.url, '../static/home.htm');
 const outputCss = await importText(import.meta.url, '../static/output.css');
 
 export function computeHomeResponse(opts: { instance: string, origin: string, productionOrigin: string, cfAnalyticsToken: string | undefined, deploySha: string | undefined, deployTime: string | undefined }): Response {
-    const { instance, origin, productionOrigin, cfAnalyticsToken, deploySha = '(undefined deploySha)', deployTime = '(undefined deployTime)' } = opts;
+    const { instance, origin, productionOrigin, cfAnalyticsToken, deploySha = 'UNDEFINEDcf1262aUNDEFINED721b53UNDEFINED', deployTime = 'UNDEFINED7T11:22:33Z' } = opts;
     const html = computeHtml(homeHtm, {
         instance,
         titleSuffix: instance === 'prod' ? '' : ` (${instance})`,
