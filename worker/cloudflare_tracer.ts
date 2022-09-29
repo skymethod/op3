c9429b6398c6386774173ef6962532dc2e10b671
John Spurlock <john@skymethod.com>
2022-09-29 11:29:44 -0500

    Index ae events by kind so less frequent but important kinds (like errors) don't get sampled.

--- worker/cloudflare_tracer.ts
+++ worker/cloudflare_tracer.ts
@@ -14,25 +14,25 @@ function computeAnalyticsEngineEvent(event: TraceEvent): AnalyticsEngineEvent {
     const { kind } = event;
     if (kind === 'error-saving-redirect') {
         const { colo, error, country, uuids } = event;
-        return { blobs: [ kind, colo, trim(error), country, trim(uuids.join(',')) ], doubles: [ 1 ] };
+        return { blobs: [ kind, colo, trim(error), country, trim(uuids.join(',')) ], doubles: [ 1 ], indexes: [ kind ] };
     } else if (kind === 'valid-redirect' || kind === 'invalid-redirect') {
         const { colo, url, country, destinationHostname } = event;
-        return { blobs: [ kind, colo, trim(url), country, trim(destinationHostname) ], doubles: [ 1 ] };
+        return { blobs: [ kind, colo, trim(url), country, trim(destinationHostname) ], doubles: [ 1 ], indexes: [ kind ] };
     } else if (kind === 'error-computing-trace-event') {
         const { error } = event;
-        return { blobs: [ kind, trim(error) ], doubles: [ 1 ] };
+        return { blobs: [ kind, trim(error) ], doubles: [ 1 ], indexes: [ kind ] };
     } else if (kind === 'worker-request') {
         const { colo, pathname, country, method, contentType, millis, status } = event;
-        return ({ blobs: [ kind, colo, trim(pathname), country, method, trim(contentType) ], doubles: [ 1, millis, status ] });
+        return ({ blobs: [ kind, colo, trim(pathname), country, method, trim(contentType) ], doubles: [ 1, millis, status ], indexes: [ kind ] });
     } else if (kind === 'do-fetch') {
         const { colo, durableObjectName, durableObjectClass, durableObjectId, isolateId, method, pathname } = event;
-        return ({ blobs: [ kind, colo, durableObjectName, durableObjectClass, durableObjectId, isolateId, method, pathname ], doubles: [ 1 ] });
+        return ({ blobs: [ kind, colo, durableObjectName, durableObjectClass, durableObjectId, isolateId, method, pathname ], doubles: [ 1 ], indexes: [ kind ] });
     } else if (kind === 'do-alarm') {
         const { colo, durableObjectName, durableObjectClass, durableObjectId, isolateId } = event;
-        return ({ blobs: [ kind, colo, durableObjectName, durableObjectClass, durableObjectId, isolateId ], doubles: [ 1 ] });
+        return ({ blobs: [ kind, colo, durableObjectName, durableObjectClass, durableObjectId, isolateId ], doubles: [ 1 ], indexes: [ kind ] });
     } else if (kind === 'console-warning' || kind === 'console-error') {
         const { spot, message } = event;
-        return ({ blobs: [ kind, spot, trim(message) ], doubles: [ 1 ] });
+        return ({ blobs: [ kind, spot, trim(message) ], doubles: [ 1 ], indexes: [ kind ] });
     } else {
         throw new Error(`CloudflareTracer: Unsupported kind: ${kind}`);
     }
--- worker/deps.ts
+++ worker/deps.ts
@@ -1,4 +1,4 @@
-export type { IncomingRequestCf, ModuleWorkerContext, DurableObjectState, DurableObjectNamespace, DurableObjectStorage, AnalyticsEngine, DurableObjectStorageValue, AnalyticsEngineEvent } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.8/common/cloudflare_workers_types.d.ts';
+export type { IncomingRequestCf, ModuleWorkerContext, DurableObjectState, DurableObjectNamespace, DurableObjectStorage, AnalyticsEngine, DurableObjectStorageValue, AnalyticsEngineEvent } from 'https://raw.githubusercontent.com/skymethod/denoflare/31db528115dc6260ac09ba2abd6c9a5aa8709b8a/common/cloudflare_workers_types.d.ts';
 export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.8/common/bytes.ts';
 export { importText } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.8/common/import_text.ts';
 export { encodeXml } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.8/common/xml_util.ts';
