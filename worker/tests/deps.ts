3589da055cb75b5900f9a5e711acc18188fe5211
John Spurlock <john@skymethod.com>
2022-10-18 11:31:19 -0500

    Update to deno 1.26.2 and latest std.

--- worker/tests/deps.ts
+++ worker/tests/deps.ts
@@ -1,2 +1,2 @@
-export { assert, assertMatch, assertEquals, assertNotEquals, assertRejects, assertThrows } from 'https://deno.land/std@0.157.0/testing/asserts.ts';
+export { assert, assertMatch, assertEquals, assertNotEquals, assertRejects, assertThrows } from 'https://deno.land/std@0.160.0/testing/asserts.ts';
 export { InMemoryDurableObjectStorage } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.9/common/storage/in_memory_durable_object_storage.ts';
