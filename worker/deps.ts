9da14ca87e3b869764a36319cfeeb40cf159b073
John Spurlock <john@skymethod.com>
2022-11-05 15:29:21 -0500

    Update to latest denoflare commit.

--- worker/deps.ts
+++ worker/deps.ts
@@ -1,8 +1,8 @@
-export type { IncomingRequestCf, ModuleWorkerContext, DurableObjectState, DurableObjectNamespace, DurableObjectStorage, DurableObjectStorageMethods, AnalyticsEngine, DurableObjectStorageValue, AnalyticsEngineEvent, DurableObjectStorageListOptions, R2Bucket, R2ListOptions } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee4e4e9f045d42b46ef73840005a425745def8c6/common/cloudflare_workers_types.d.ts';
-export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee4e4e9f045d42b46ef73840005a425745def8c6/common/bytes.ts';
-export { importText } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee4e4e9f045d42b46ef73840005a425745def8c6/common/import_text.ts';
-export { encodeXml } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee4e4e9f045d42b46ef73840005a425745def8c6/common/xml_util.ts';
-export { setEqual, setIntersect, setSubtract } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee4e4e9f045d42b46ef73840005a425745def8c6/common/sets.ts';
+export type { IncomingRequestCf, ModuleWorkerContext, DurableObjectState, DurableObjectNamespace, DurableObjectStorage, DurableObjectStorageMethods, AnalyticsEngine, DurableObjectStorageValue, AnalyticsEngineEvent, DurableObjectStorageListOptions, R2Bucket, R2ListOptions } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee31d830efb6971ed91f5e91bec7769177337e9e/common/cloudflare_workers_types.d.ts';
+export { Bytes } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee31d830efb6971ed91f5e91bec7769177337e9e/common/bytes.ts';
+export { importText } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee31d830efb6971ed91f5e91bec7769177337e9e/common/import_text.ts';
+export { encodeXml } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee31d830efb6971ed91f5e91bec7769177337e9e/common/xml_util.ts';
+export { setEqual, setIntersect, setSubtract } from 'https://raw.githubusercontent.com/skymethod/denoflare/ee31d830efb6971ed91f5e91bec7769177337e9e/common/sets.ts';
 export { chunk } from 'https://deno.land/std@0.162.0/collections/chunk.ts';
 export { XMLParser } from 'https://cdn.skypack.dev/fast-xml-parser@4.0.11?dts';
 export { distinct } from 'https://deno.land/std@0.162.0/collections/distinct.ts';
