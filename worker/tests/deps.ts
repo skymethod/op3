3b4ed3a43b37046bce4f76778e43cdfad410fcd3
John Spurlock <john@skymethod.com>
2022-11-13 09:09:26 -0600

    User agents and tests.

--- worker/tests/deps.ts
+++ worker/tests/deps.ts
@@ -1,2 +1,2 @@
-export { assert, assertMatch, assertEquals, assertNotEquals, assertRejects, assertThrows } from 'https://deno.land/std@0.163.0/testing/asserts.ts';
+export { assert, assertMatch, assertEquals, assertNotEquals, assertRejects, assertThrows, fail } from 'https://deno.land/std@0.163.0/testing/asserts.ts';
 export { InMemoryDurableObjectStorage } from 'https://raw.githubusercontent.com/skymethod/denoflare/v0.5.10/common/storage/in_memory_durable_object_storage.ts';
