5b16dcdf27c7057eb4d2e444e1706632ae4e6c7b
John Spurlock <john@skymethod.com>
2022-11-29 12:26:16 -0600

    Enable logpush for worker to track unhandled exceptions.

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ NO_COLOR=1 DENO_VERSION=$DENO_VERSION DENOFLARE_VERSION=${DENOFLARE_VERSION} ./d
 
 # denoflare push the worker script to cloudflare
 NO_COLOR=1 DENO_VERSION=$DENO_VERSION DENOFLARE_VERSION=${DENOFLARE_VERSION} ./deno-$DENO_VERSION/bin/deno run --unstable --allow-all https://raw.githubusercontent.com/skymethod/denoflare/$DENOFLARE_VERSION/cli/cli.ts \
-push ./worker/worker.ts --account-id $CF_ACCOUNT_ID --api-token $CF_API_TOKEN --custom-domain $CF_CUSTOM_DOMAIN --name $CF_SCRIPT_NAME \
+push ./worker/worker.ts --account-id $CF_ACCOUNT_ID --api-token $CF_API_TOKEN --custom-domain $CF_CUSTOM_DOMAIN --name $CF_SCRIPT_NAME --logpush \
 --text-binding instance:$INSTANCE \
 --text-binding deployTime:$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
 --text-binding deployRepositoryUrl:$DEPLOY_REPOSITORY_URL \
