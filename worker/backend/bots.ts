2ef9970f83354121988effcaa0c29bf8bd142280
John Spurlock <john@skymethod.com>
2023-01-09 17:10:42 -0600

    Agent name should be the blank check.

--- worker/backend/bots.ts
+++ worker/backend/bots.ts
@@ -4,7 +4,7 @@ export function computeBotType({ agentType, agentName = '', deviceType, referrer
     if (agentType === 'bot') return 'bot'; // easy
     if (agentType === 'bot-library') return 'bot-lib'; // user-agents-v2 type='library' with category='bot'
     if (agentType === 'unknown' && /(bot|crawler)/i.test(agentName)) return 'unknown-bot'; // "bot" or "crawler" in the user-agent string for unknown agents
-    if (agentType === '') return 'no-ua'; // no user-agent header provided in the request (or blank) - majority found from Amazon IPs
+    if (agentName === '') return 'no-ua'; // no user-agent header provided in the request (or blank) - majority found from Amazon IPs
 
     // 2022-12-12: Observed Opera desktop pre-downloading all enclosures for rss feeds added to "My Sources" - they have no referrer
     if (agentType === 'browser' && agentName === 'Opera' && deviceType === 'computer' && referrerName === undefined) return 'opera-desktop-sans-referrer';
