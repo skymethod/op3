0fd75f2f8722eabebb1a9f82c182cd15fc7384d0
John Spurlock <john@skymethod.com>
2023-01-09 16:36:57 -0600

    Consider an unknown UA containing 'crawler' a bot.

--- worker/backend/bots.ts
+++ worker/backend/bots.ts
@@ -3,7 +3,7 @@ export type BotType = 'bot' | 'bot-lib' | 'unknown-bot' | 'opera-desktop-sans-re
 export function computeBotType({ agentType, agentName = '', deviceType, referrerName }: { agentType: string, agentName?: string, deviceType?: string, referrerName?: string }): BotType | undefined {
     if (agentType === 'bot') return 'bot'; // easy
     if (agentType === 'library' && isBotLibrary(agentName)) return 'bot-lib'; // some libraries are used in listener apps for playback
-    if (agentType === 'unknown' && /bot/i.test(agentName)) return 'unknown-bot'; // "bot" in the user-agent string for unknown agents
+    if (agentType === 'unknown' && /(bot|crawler)/i.test(agentName)) return 'unknown-bot'; // "bot" or "crawler" in the user-agent string for unknown agents
 
     // 2022-12-12: Observed Opera desktop pre-downloading all enclosures for rss feeds added to "My Sources" - they have no referrer
     if (agentType === 'browser' && agentName === 'Opera' && deviceType === 'computer' && referrerName === undefined) return 'opera-desktop-sans-referrer';
