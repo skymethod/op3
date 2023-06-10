63980b17165bedb025c18d13e6708117f3701e97
John Spurlock <john@skymethod.com>
2023-06-10 10:21:16 -0500

    New Cloudflare invoice.

--- worker/routes/costs.ts
+++ worker/routes/costs.ts
@@ -42,6 +42,21 @@ interface CostDetail {
 
 const COSTS: Cost[] = [
     {
+        date: '2023-06-10',
+        description: 'Cloudflare invoice',
+        cost: 400.91,
+        detail: [
+            { description: 'Workers Paid Subscription', cost: 5.00 },
+            { description: 'Workers requests', cost: 24.10 },
+            { description: 'Durable Objects Compute', cost: 12.50 },
+            { description: 'Durable Objects Reads/Writes/Deletes', cost: 270.20 },
+            { description: 'Durable Objects Storage', cost: 53.60 },
+            { description: 'R2 Data Storage', cost: 0.68 },
+            { description: 'Advanced Certificate Manager (op3.st)', cost: 10.00 },
+            { description: 'Sales Tax', cost: 24.83 },
+        ]
+    },
+    {
         date: '2023-05-10',
         description: 'Cloudflare invoice',
         cost: 372.09,
