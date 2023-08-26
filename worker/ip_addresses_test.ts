1bf4c942093bcab6ad006588d53be0570abf6865
John Spurlock <john@skymethod.com>
2023-08-26 12:43:40 -0500

    Migrate from std/testing -> std.assert.

--- worker/ip_addresses_test.ts
+++ worker/ip_addresses_test.ts
@@ -3,7 +3,7 @@ import { Bytes } from './deps.ts';
 import { packHashedIpAddress, unpackHashedIpAddressHash } from './ip_addresses.ts';
 
 Deno.test({
-    name: 'IsolateId.get',
+    name: 'ipaddress-packing',
     fn: () => {
         const signature = Bytes.ofUtf8('whatever');
         assertEquals(unpackHashedIpAddressHash(packHashedIpAddress('1', signature)), signature.hex());
