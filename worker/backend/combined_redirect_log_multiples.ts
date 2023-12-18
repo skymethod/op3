72490919cdbbf06103c34ee2d67b2e0c0066b65d
John Spurlock <john@skymethod.com>
2023-12-18 17:03:16 -0600

    Boost the most behind.

--- worker/backend/combined_redirect_log_multiples.ts
+++ worker/backend/combined_redirect_log_multiples.ts
@@ -2,11 +2,11 @@ import { DoNames } from '../do_names.ts';
 
 export const MULTIPLES = Object.fromEntries([
     [ DoNames.redirectLogForColo('AMS'), 4 ],
-    [ DoNames.redirectLogForColo('ORD'), 2 ],
-    [ DoNames.redirectLogForColo('SEA'), 2 ],
+    [ DoNames.redirectLogForColo('ORD'), 8 ],
+    [ DoNames.redirectLogForColo('SEA'), 8 ],
     [ DoNames.redirectLogForColo('DFW'), 8 ],
-    [ DoNames.redirectLogForColo('SJC'), 8 ],
+    [ DoNames.redirectLogForColo('SJC'), 2 ],
     [ DoNames.redirectLogForColo('LAX'), 2 ],
     [ DoNames.redirectLogForColo('IAD'), 2 ],
-    [ DoNames.redirectLogForColo('EWR'), 2 ],
+    [ DoNames.redirectLogForColo('EWR'), 8 ],
 ]);
