e7184b66c6f8f88debf9a5fbfba56c0caf0ab63c
John Spurlock <john@skymethod.com>
2023-12-18 14:19:11 -0600

    Boost DFW/SJC/LAX/IAD/EWR 2x.

--- worker/backend/combined_redirect_log_multiples.ts
+++ worker/backend/combined_redirect_log_multiples.ts
@@ -4,4 +4,9 @@ export const MULTIPLES = Object.fromEntries([
     [ DoNames.redirectLogForColo('AMS'), 4 ],
     [ DoNames.redirectLogForColo('ORD'), 2 ],
     [ DoNames.redirectLogForColo('SEA'), 2 ],
+    [ DoNames.redirectLogForColo('DFW'), 2 ],
+    [ DoNames.redirectLogForColo('SJC'), 2 ],
+    [ DoNames.redirectLogForColo('LAX'), 2 ],
+    [ DoNames.redirectLogForColo('IAD'), 2 ],
+    [ DoNames.redirectLogForColo('EWR'), 2 ],
 ]);
