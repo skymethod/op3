8447288facbd6623777aa3e3089f6e1da5967fef
John Spurlock <john@skymethod.com>
2023-12-18 20:03:40 -0600

    New top 4 and ease off.

--- worker/backend/combined_redirect_log_multiples.ts
+++ worker/backend/combined_redirect_log_multiples.ts
@@ -1,8 +1,8 @@
 import { DoNames } from '../do_names.ts';
 
 export const MULTIPLES = Object.fromEntries([
-    [ DoNames.redirectLogForColo('AMS'), 16 ],
-    [ DoNames.redirectLogForColo('DFW'), 8 ],
-    [ DoNames.redirectLogForColo('EWR'), 8 ],
-    [ DoNames.redirectLogForColo('IAD'), 8 ],
+    [ DoNames.redirectLogForColo('HEL'), 2 ],
+    [ DoNames.redirectLogForColo('ORD'), 4 ],
+    [ DoNames.redirectLogForColo('AMS'), 4 ],
+    [ DoNames.redirectLogForColo('DFW'), 4 ],
 ]);
