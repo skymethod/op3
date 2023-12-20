fac0bb6e262b114234bd494c4f5d286476cac2f0
John Spurlock <john@skymethod.com>
2023-12-20 15:41:35 -0600

    Reset all boosts.

--- worker/backend/combined_redirect_log_multiples.ts
+++ worker/backend/combined_redirect_log_multiples.ts
@@ -1,8 +1,5 @@
 import { DoNames } from '../do_names.ts';
 
 export const MULTIPLES = Object.fromEntries([
-    [ DoNames.redirectLogForColo('HEL'), 2 ],
-    [ DoNames.redirectLogForColo('ORD'), 4 ],
-    [ DoNames.redirectLogForColo('AMS'), 4 ],
-    [ DoNames.redirectLogForColo('DFW'), 4 ],
+    [ DoNames.redirectLogForColo('AMS'), 1 ],
 ]);
