8f86c00625ac86f966e1ad803a7ea3caaebaad39
John Spurlock <john@skymethod.com>
2023-12-23 18:52:17 -0600

    Boost EWR 2x.

--- worker/backend/combined_redirect_log_multiples.ts
+++ worker/backend/combined_redirect_log_multiples.ts
@@ -2,4 +2,5 @@ import { DoNames } from '../do_names.ts';
 
 export const MULTIPLES = Object.fromEntries([
     [ DoNames.redirectLogForColo('AMS'), 2 ],
+    [ DoNames.redirectLogForColo('EWR'), 2 ],
 ]);
