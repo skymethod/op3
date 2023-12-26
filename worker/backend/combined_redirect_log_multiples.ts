c5f6622086ab06ef8c4b0ef5859323558a305c34
John Spurlock <john@skymethod.com>
2023-12-26 12:50:05 -0600

    Boost ORD 2x.

--- worker/backend/combined_redirect_log_multiples.ts
+++ worker/backend/combined_redirect_log_multiples.ts
@@ -3,4 +3,5 @@ import { DoNames } from '../do_names.ts';
 export const MULTIPLES = Object.fromEntries([
     [ DoNames.redirectLogForColo('AMS'), 2 ],
     [ DoNames.redirectLogForColo('EWR'), 2 ],
+    [ DoNames.redirectLogForColo('ORD'), 2 ],
 ]);
