52296cbc8c95c96dd9b7cdfe995513a568274e65
John Spurlock <john@skymethod.com>
2024-01-11 19:47:06 -0600

    Boost ORD, DFW 4x.

--- worker/backend/combined_redirect_log_multiples.ts
+++ worker/backend/combined_redirect_log_multiples.ts
@@ -3,5 +3,6 @@ import { DoNames } from '../do_names.ts';
 export const MULTIPLES = Object.fromEntries([
     [ DoNames.redirectLogForColo('AMS'), 2 ],
     [ DoNames.redirectLogForColo('EWR'), 2 ],
-    [ DoNames.redirectLogForColo('ORD'), 12 ],
+    [ DoNames.redirectLogForColo('ORD'), 4 ],
+    [ DoNames.redirectLogForColo('DFW'), 4 ],
 ]);
