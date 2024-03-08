bbd98bbd2589248cf94c19defcb741262c57212a
John Spurlock <john@skymethod.com>
2024-03-08 16:28:18 -0600

    Exclude Podverse web plays from stats, found to preload audio.

--- worker/backend/bots.ts
+++ worker/backend/bots.ts
@@ -1,4 +1,4 @@
-export type BotType = 'bot' | 'bot-lib' | 'unknown-bot' | 'opera-desktop-sans-referrer' | 'no-ua';
+export type BotType = 'bot' | 'bot-lib' | 'unknown-bot' | 'opera-desktop-sans-referrer' | 'no-ua' | 'podverse-web-preload';
 
 export function computeBotType({ agentType, agentName = '', deviceType, referrerName }: { agentType: string, agentName?: string, deviceType?: string, referrerName?: string }): BotType | undefined {
     if (agentType === 'bot') return 'bot'; // easy
@@ -8,4 +8,7 @@ export function computeBotType({ agentType, agentName = '', deviceType, referrer
 
     // 2022-12-12: Observed Opera desktop pre-downloading all enclosures for rss feeds added to "My Sources" - they have no referrer
     if (agentType === 'browser' && agentName === 'Opera' && deviceType === 'computer' && referrerName === undefined) return 'opera-desktop-sans-referrer';
+
+    // 2024-03-08: Observed Podverse embedded player widget and first-party player requesting the entire file before user playback (preload="metadata")
+    if (agentType === 'browser' && referrerName === 'Podverse') return 'podverse-web-preload';
 }
