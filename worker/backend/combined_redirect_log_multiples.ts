2a64c09ef966bec048afd6d61ec9104efa8b6263
John Spurlock <john@skymethod.com>
2024-04-14 10:53:29 -0500

    Unboost LAX. Boost AMS x4.

--- worker/backend/combined_redirect_log_multiples.ts
+++ worker/backend/combined_redirect_log_multiples.ts
@@ -1,10 +1,10 @@
 import { DoNames } from '../do_names.ts';
 
 export const MULTIPLES = Object.fromEntries([
-    [ DoNames.redirectLogForColo('AMS'), 2 ],
+    [ DoNames.redirectLogForColo('AMS'), 4 ],
     [ DoNames.redirectLogForColo('EWR'), 2 ],
     [ DoNames.redirectLogForColo('ORD'), 4 ],
     [ DoNames.redirectLogForColo('DFW'), 4 ],
-    [ DoNames.redirectLogForColo('LAX'), 4 ],
+    // [ DoNames.redirectLogForColo('LAX'), 4 ],
     [ DoNames.redirectLogForColo('CNN'), 8 ],
 ]);
