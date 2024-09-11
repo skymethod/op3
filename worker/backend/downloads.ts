d2f728040090f17915059e2b337f77adfe67f3f7
John Spurlock <john@skymethod.com>
2024-09-11 09:38:51 -0500

    New bot ip.

--- worker/backend/downloads.ts
+++ worker/backend/downloads.ts
@@ -125,6 +125,7 @@ const botIps = new Set([
     'c1cf85ed0bcb71afdc52ce52ad54cba30cf05a7e', // 2024-08-21 for 2024-08-19
     '055339109b8a6d1c0f508fb491fec8ca54526748', // 2024-08-21 for 2024-08-19
     '9d966be7e654e5404f0588652bfab78b58dfe3d4', // 2024-08-26 for 2024-08-25
+    '30f72fe4ce0f92c2f65d67e8a77b46b28f7375f2', // 2024-09-11 for 2024-09-10
 ]);
 
 // phase 2: process a day's worth of hourly download blobs, compute final downloads and assign to zero or one shows, save as 24 associated column blobs (partitioned)
