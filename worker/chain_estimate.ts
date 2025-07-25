1a4bd7510d65980ff37cd6cbb434c3a14dc49c23
John Spurlock <john@skymethod.com>
2025-07-25 15:50:20 -0400

    Update chain estimate with workaround.

--- worker/chain_estimate.ts
+++ worker/chain_estimate.ts
@@ -18,7 +18,7 @@ export function computeChainEstimate(url: string): ChainEstimate {
 
     // https://op3.dev/e(,args)?/(https?://)?
     // no http support (.dev TLD on HSTS preload list), but clients (browsers) redirect anyway?
-    let m = /^https?:\/\/(ci\.|staging\.)?\op3\.dev(:443|:80)?\/e(,.*?)?\/\/?(.+?)$/.exec(url);
+    let m = /^https?:\/\/(ci\.|staging\.)?\op3\.dev(:443|:80)?\/e(,.*?|\/pg=[^/]+)?\/\/?(.+?)$/.exec(url);
     if (m) {
         const [ _, _subdomain, _port, _args, suffix ] = m;
         m = /^((https?):\/\/?).*?$/i.exec(suffix);
--- worker/chain_estimate_test.ts
+++ worker/chain_estimate_test.ts
@@ -438,6 +438,11 @@ Deno.test({
             { kind: 'prefix', prefix: 'ppprotect', url: 'https://pp1.example.com/protect/example/a.com/path/to/episode.mp3' },
             { kind: 'destination', url: 'https://a.com/path/to/episode.mp3' }
         ]);
+
+        assertEquals(computeChainEstimate('https://op3.dev/e/pg=bbda7016-db7b-4e3d-8bee-22c14bb71457/a.com/path/to/episode.mp3'), [
+            { kind: 'prefix', prefix: 'op3', url: 'https://op3.dev/e/pg=bbda7016-db7b-4e3d-8bee-22c14bb71457/a.com/path/to/episode.mp3' },
+            { kind: 'destination', url: 'https://a.com/path/to/episode.mp3' }
+        ]);
     }
 });
 
