20b842de99cc73945110b9dbf1626913672ea67d
John Spurlock <john@skymethod.com>
2025-10-26 16:37:29 -0400

    New micro.blog invoice.

--- worker/routes/costs.ts
+++ worker/routes/costs.ts
@@ -42,6 +42,14 @@ interface CostDetail {
 
 const COSTS: Cost[] = [
     {
+        date: '2025-10-26',
+        description: 'Micro.blog invoice',
+        cost: 5.00,
+        detail: [
+            { description: 'micro.blog 1 month', cost: 5.00 },
+        ]
+    },
+    {
         date: '2025-10-10',
         description: 'Cloudflare invoice',
         cost: 535.67,
