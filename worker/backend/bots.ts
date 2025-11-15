24d84041a90da0aaf194348d43806ba8c724f2a4
John Spurlock <john@skymethod.com>
2025-11-15 09:41:32 -0500

    New bot ip hash.

--- worker/backend/bots.ts
+++ worker/backend/bots.ts
@@ -267,4 +267,5 @@ const botIpHashes = new Set([
     'cf9657f886e0a050be8215e69d4bcbde752a65c0', // 2025-11-13 for 2025-11-12 12876  scaleway MPlayer, Windows Media Player
     '4c4f0fb17f89fbf2fb69b2be8ef9c0030fa1753c', // 2025-11-14 for 2025-11-13 812    rogers   Overcast
     '7cb4d07418e059efab22f05002e96ce6b4f635e2', // 2025-11-14 for 2025-11-13 812    rogers   Overcast
+    '31080a7abab9047e56ee06be96b98b8780beff83', // 2025-11-15 for 2025-11-14 212238 datacamp Chrome
 ]);
