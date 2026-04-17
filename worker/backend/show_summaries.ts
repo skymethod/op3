7f254423cb22aec4251b08fb8ce6c7449f01cd47
John Spurlock <john@skymethod.com>
2026-04-17 09:40:40 -0400

    Still happening but losing trace.

--- worker/backend/show_summaries.ts
+++ worker/backend/show_summaries.ts
@@ -113,7 +113,7 @@ export async function computeShowSummaryAggregate({ showUuid, inputKeys, outputP
     const sources: Record<string, string> = {};
     const episodes: Record<string, EpisodeSummary> = {};
     const dimensionDownloads: Record<string, Record<string, number>> = {};
-    const results = await Promise.all(inputKeys.map(async v => ({ key: v, result: await executeWithRetries(() => statsBlobs.get(v, 'text-and-meta'), { tag: `stats-blobs-get-text-meta-${v}`, maxRetries: 3, isRetryable: e => {
+    const results = await Promise.all(inputKeys.map(async v => ({ key: v, result: await executeWithRetries(async () => await statsBlobs.get(v, 'text-and-meta'), { tag: `stats-blobs-get-text-meta-${v}`, maxRetries: 3, isRetryable: e => {
         const m = `${(e as Error).stack ?? e}`;
         if (m.includes('TypeError: error reading a body from connection')) return true; // TypeError: error reading a body from connection
         return false;
