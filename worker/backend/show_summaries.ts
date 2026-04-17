b83ca1fa166f4fe853e0abf62dbdfd7fa85d0594
John Spurlock <john@skymethod.com>
2026-04-17 09:30:48 -0400

    Workaround new r2 timeout issue.

--- worker/backend/show_summaries.ts
+++ worker/backend/show_summaries.ts
@@ -8,6 +8,7 @@ import { isValidUuid } from '../uuid.ts';
 import { timed } from '../async.ts';
 import { computeTimestamp, unpackDate } from '../timestamp.ts';
 import { recomputeAudienceForMonth, saveAudience } from './audience.ts';
+import { executeWithRetries } from '../sleep.ts';
 
 export type RecomputeShowSummariesForMonthRequest = { showUuid: string, month: string, log?: boolean, sequential?: boolean, startDay?: number, maxDays?: number, phases?: Phase[] };
 
@@ -112,7 +113,11 @@ export async function computeShowSummaryAggregate({ showUuid, inputKeys, outputP
     const sources: Record<string, string> = {};
     const episodes: Record<string, EpisodeSummary> = {};
     const dimensionDownloads: Record<string, Record<string, number>> = {};
-    const results = await Promise.all(inputKeys.map(async v => ({ key: v, result: await statsBlobs.get(v, 'text-and-meta') })));
+    const results = await Promise.all(inputKeys.map(async v => ({ key: v, result: await executeWithRetries(() => statsBlobs.get(v, 'text-and-meta'), { tag: `stats-blobs-get-text-meta-${v}`, maxRetries: 3, isRetryable: e => {
+        const m = `${(e as Error).stack ?? e}`;
+        if (m.includes('TypeError: error reading a body from connection')) return true; // TypeError: error reading a body from connection
+        return false;
+    } }) })));
     for (const { key, result } of results) {
         if (result === undefined) continue;
         const { text, etag } = result;
